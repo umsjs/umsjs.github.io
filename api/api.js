1364f39ee9ed0bcd8f33ac0c7d1df429a8973569
umsjs <ultrant@126.com>
2014-10-14 18:00:29 +0800

    add api documents

---
+++ api/api.js
@@ -0,0 +1,30 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "EventEmitterEx",
+        "ums",
+        "ums.Application",
+        "ums.NetConnection",
+        "ums.NetStream",
+        "ums.PublishPipe",
+        "ums.PublishPool",
+        "ums.RTMPCall",
+        "ums.RTMPClient",
+        "ums.RTMPConnector",
+        "ums.RTMPOptions",
+        "ums.RTMPServer",
+        "ums.RTSPConnection",
+        "ums.RTSPConnector"
+    ],
+    "modules": [
+        "ums.js"
+    ],
+    "allModules": [
+        {
+            "displayName": "ums.js",
+            "name": "ums.js",
+            "description": "javascript glue wrapper of umsjs module"
+        }
+    ]
+} };
+});
